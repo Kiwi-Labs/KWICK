a06f378c33a41facfd16aeffa29773e42b0cb29a
William Brandon <hypercube97@gmail.com>
2015-07-25 00:31:22 -0700

    Improved main function to allow parsing whole files
    The current main function is completely temporary and exists only for
    the purpose of debugging the parser, but it is nonetheless nice to be
    able to test whole files, rather than only being able to work with
    single lines.
    
    Files to parse can be specified as a command-line argument to the
    executable.  When a file is not specified, the executable defaults to
    the old behavior of reading single lines as modules.  The file-to-parse
    argument cannot be specified when the executable is invoked through
    “./run.sh”

--- kwick/parser/main.hs
+++ kwick/parser/main.hs
@@ -2,5 +2,12 @@ module Main where
 
 import Parse
 import ParseModule
+import Control.Monad (forever)
+import System.Environment (getArgs)
 
-main = getLine >>= (print . runParse parseModule) >> main
+main = do
+	args <- getArgs
+	case args of
+		[] -> forever $ getLine >>= (print . runParse parseModule)
+		[filename] -> readFile filename >>= (print . runParse parseModule)
+		_ -> putStrLn "Wrong number of command-line arguments!"
